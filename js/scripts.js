7c2d89a03c291f249bde6b99482decbcf041955b
Piotr Sociński <psocinski@gmail.com>
2018-02-27 20:12:03 +0100

    New view

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 // Find the 'count this' class on the page and animate it
 $('.count-this').each(function () {
-
-  // Start the counting from a specified number - in this case, 0!
-  $(this).prop('Counter',0).animate({
-      Counter: $(this).text()
-  }, {
-      // Speed of counter in ms, default animation style
-      duration: 6000,
-      // Easing function
-      easing: 'swing',
-      step: function (now) {
+    // Start the counting from a specified number - in this case, 0!
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+        }, {
+        // Speed of counter in ms, default animation style
+        duration: 6000,
+        // Easing function
+        easing: 'swing',
+        step: function (now) {
         // Round up the number
-          $(this).text(Math.ceil(now));
-      }
-  });
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+// Płynne przewijanie scroll
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 900);
+    }
 });
