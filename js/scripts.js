a144d8392e0e62191e1767cc2595f71631a5ed16
Piotr Sociński <psocinski@gmail.com>
2018-02-28 15:40:03 +0100

    Finished two section :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,57 @@
+
+// Płynne przewijanie scroll
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 900);
+    }
+});
+
+// Licznik z scrollem // Main counter :)
+$('.counter').countUp({
+    delay: 20,
+    time: 3000,
+});
+
+// Licznik 1
+// var a = 0;
+// $(window).scroll(function() {
+
+//   var oTop = $('#counter').offset().top - window.innerHeight;
+//   if (a == 0 && $(window).scrollTop() > oTop) {
+//     $('.counter-value').each(function() {
+//       var $this = $(this),
+//         countTo = $this.attr('data-count');
+//       $({
+//         countNum: $this.text()
+//       }).animate({
+//           countNum: countTo
+//         },
+
+//         {
+
+//           duration: 3000,
+//           easing: 'swing',
+//           step: function() {
+//             $this.text(Math.floor(this.countNum));
+//           },
+//           complete: function() {
+//             $this.text(this.countNum);
+//             //alert('finished');
+//           }
+
+//         });
+//     });
+//     a = 1;
+//   }
+
+// });
+
+
+// Licznik 2
 // // Find the 'count this' class on the page and animate it
 // $('.count-this').each(function () {
 //     // Start the counting from a specified number - in this case, 0!
@@ -14,48 +68,3 @@
 //         }
 //     });
 // });
-
-// Płynne przewijanie scroll
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 900);
-    }
-});
-
-// Licznik 
-var a = 0;
-$(window).scroll(function() {
-
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 3000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-
-        });
-    });
-    a = 1;
-  }
-
-});
